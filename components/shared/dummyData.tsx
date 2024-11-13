39d388c5effe65a08875fc5bf936c36ec44989c5
EbukaOkoye <chukwuebukaokoye408@gmail.com>
2024-11-14 00:20:29 +0100

    marketplace home page and single view page done.... checkout page will be completed before weekend

--- components/shared/dummyData.tsx
+++ components/shared/dummyData.tsx
@@ -3,60 +3,83 @@ import { offlineImage } from "@/constants/images";
 export const foodTypes = [
     {
         image: offlineImage.salad,
-        name: 'Food',
+        name: "Food",
     },
     {
         image: offlineImage.shirt,
-        name: 'Fashion',
+        name: "Fashion",
     },
     {
         image: offlineImage.laptop,
-        name: 'Gadgets',
+        name: "Gadgets",
     },
     {
         image: offlineImage.salad,
-        name: 'Food',
+        name: "Food",
     },
     {
         image: offlineImage.laptop,
-        name: 'Gadgets',
+        name: "Gadgets",
     },
 ];
 
 export const products = [
     {
         img: offlineImage.emptyCardImg,
-        title: 'AIRism Short Sleeve Ultra Stretch Crop',
-        price: '$12.63',
+        title: "AIRism Short Sleeve Ultra Stretch Crop",
+        price: "$12.63",
     },
     {
         img: offlineImage.emptyCardImg,
-        title: 'Comfeel Touch Casual Shoes',
-        price: '$26.42',
+        title: "Comfeel Touch Casual Shoes",
+        price: "$26.42",
     },
     {
         img: offlineImage.emptyCardImg,
-        title: 'Synthetic Leather Round Shoulder Bag',
-        price: '$30.42',
+        title: "Synthetic Leather Round Shoulder Bag",
+        price: "$30.42",
     },
     {
         img: offlineImage.emptyCardImg,
-        title: 'AIRism Short Sleeve Ultra Stretch Crop',
-        price: '$12.63',
+        title: "AIRism Short Sleeve Ultra Stretch Crop",
+        price: "$12.63",
     },
     {
         img: offlineImage.emptyCardImg,
-        title: 'Comfeel Touch Casual Shoes',
-        price: '$26.42',
+        title: "Comfeel Touch Casual Shoes",
+        price: "$26.42",
     },
     {
         img: offlineImage.emptyCardImg,
-        title: 'AIRism Short Sleeve Ultra Stretch Crop',
-        price: '$12.63',
+        title: "AIRism Short Sleeve Ultra Stretch Crop",
+        price: "$12.63",
     },
     {
         img: offlineImage.emptyCardImg,
-        title: 'Comfeel Touch Casual Shoes',
-        price: '$26.42',
+        title: "Comfeel Touch Casual Shoes",
+        price: "$26.42",
     },
 ];
+
+export const carouselImg = [
+    offlineImage.emptyCardImg,
+    offlineImage.emptyCardImg,
+    offlineImage.emptyCardImg,
+];
+
+export const detailsCarousel = [
+    { uri: offlineImage.emptyCardImg },
+    { uri: offlineImage.emptyCardImg },
+    { uri: offlineImage.emptyCardImg },
+];
+
+export const colors = [
+    "#ECECEC",
+    "#2D2F2E",
+    "#DFD6D1",
+    "#998F8E",
+    "#394764",
+    "#B8ACC0",
+];
+
+export const sizes = ['S', 'M', 'L', 'XL'];
